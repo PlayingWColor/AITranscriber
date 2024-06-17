d02087ad5114e2a612c25053ebc1b7e5a96b35d5
PlayingWColor <playingwcolor@gmail.com>
2024-06-16 23:19:04 -0400

    added RUN bat file and settings json

---
+++ RUN.bat
@@ -0,0 +1,3 @@
+@echo off
+python transcriber.py
+pause
--- transcriber.py
+++ transcriber.py
@@ -1,18 +1,24 @@
 import whisper
 import os
+import json
+
+settings_file = open("settings.json")
+settings = json.load(settings_file)
+settings_file.close()
 
 in_dir = "./in/"
 out_dir = "./out/"
 
 all_files = [file for file in os.listdir(in_dir)]
 
-#print(all_files)
-
-model = whisper.load_model("base")
+model = whisper.load_model(settings["model"])
 
 for file in all_files:
+    if file[0] == '.':
+        continue
+
     result = model.transcribe(in_dir+file)
-    print(result["text"])
+    print("Finished! Check the \"out\" folder to see the results.")
 
     output = open(out_dir+file[:-4]+".txt", 'w', encoding="utf-8")
     output.write("".join([s["text"]+"\n" for s in result["segments"]]))
